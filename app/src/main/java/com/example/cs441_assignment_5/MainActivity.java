695f0ad2ac879574e34765f5f52c4b50c1af7547
Richard Zheng <Richard@Richards-MacBook-Pro-5.local>
2019-04-01 15:33:02 -0400

    more draws

--- app/src/main/java/com/example/cs441_assignment_5/MainActivity.java
+++ app/src/main/java/com/example/cs441_assignment_5/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
