b15e78d8e7bce38a8c5498a2e3ad37f18134a875
Taehyun Cho <taehyuncho@Taehyuns-MacBook-Air.local>
2019-04-01 16:43:37 -0400

    Added Ant class/Object details

--- app/src/main/java/com/example/cs441_assignment_5/MainActivity.java
+++ app/src/main/java/com/example/cs441_assignment_5/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.cs441_assignment_5;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+class ANT{
+    public float xCord;
+    public float yCord;
+    public int health = 100;
+    public int level = 1;
+}
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
