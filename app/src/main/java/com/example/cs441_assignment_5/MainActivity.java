6aae6b3a9e6f272a607bb52f06a8283d396d02a4
Taehyun Cho <taehyuncho@Taehyuns-MacBook-Air.local>
2019-04-01 17:38:17 -0400

    Implemented ImageView for background map. Edited file names to run properly.

--- app/src/main/java/com/example/cs441_assignment_5/MainActivity.java
+++ app/src/main/java/com/example/cs441_assignment_5/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.cs441_assignment_5;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 
 class ANT{
     public float xCord;
@@ -16,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Gets rid of status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         //this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
