3722d068cae20e3e961c4aa4ab4f789968855aaf
Taehyun Cho <taehyuncho@Taehyuns-MacBook-Air.local>
2019-04-04 15:53:09 -0400

    Added the shop and score views. Cleaned up main activity, tower drags work

--- app/src/main/java/com/example/cs441_assignment_5/MainActivity.java
+++ app/src/main/java/com/example/cs441_assignment_5/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Gallery;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     RelativeLayout newLayout; //layout
     private int _xDelta; //x variable
     private int _yDelta; //y variable
+    private float dX;
+    private float dY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,79 +36,31 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         //Gets rid of status bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        //this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
-
-        //creates a new layout
-        newLayout = new RelativeLayout(this);
-
-        //sets background
-        newLayout.setBackgroundResource(R.drawable.antmap);
-
-        //creates new object
-        ImageView tower1 = new ImageView(this);
-        tower1.setImageResource(R.drawable.tower1);
-
-        //defines item coordinates and size.
-        tower1.setX(200);
-        tower1.setY(600);
-        tower1.setMaxHeight(200);
-        tower1.setMaxWidth(200);
-        tower1.setAdjustViewBounds(true);
-        tower1.setLayoutParams(new Gallery.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-
-        //allows listener
+        ImageButton shopButton = findViewById(R.id.shopButton);
+        ImageView tower1 = findViewById(R.id.tower1);
         tower1.setOnTouchListener(this);
 
-        //adds item to view
-        newLayout.addView(tower1);
-        //sets view
-        setContentView(newLayout);
-
     }
-
+    @Override
     public boolean onTouch(View view, MotionEvent event) {
-        final int X = (int) event.getRawX();
-        final int Y = (int) event.getRawY();
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                _xDelta = X - lParams.leftMargin;
-                _yDelta = Y - lParams.topMargin;
+                dX = view.getX() - event.getRawX();
+                dY = view.getY() - event.getRawY();
                 break;
-            case MotionEvent.ACTION_UP:
-                break;
-            case MotionEvent.ACTION_POINTER_DOWN:
-                break;
-            case MotionEvent.ACTION_POINTER_UP:
-                break;
+
             case MotionEvent.ACTION_MOVE:
-                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
-                        .getLayoutParams();
-                layoutParams.leftMargin = X - _xDelta;
-                layoutParams.topMargin = Y - _yDelta;
-                layoutParams.rightMargin = -250;
-                layoutParams.bottomMargin = -250;
-                view.setLayoutParams(layoutParams);
-
-
-                //makes a new Tower in the same spot
-                ImageView newTower1 = new ImageView(this);
-                newTower1.setImageResource(R.drawable.tower1);
-                newTower1.setAdjustViewBounds(true);
-                newTower1.setX(200);
-                newTower1.setY(600);
-                newTower1.setMaxHeight(200);
-                newTower1.setMaxWidth(200);
-                newTower1.setOnTouchListener(this);
-
-                // Adds the view to the layout
-                newLayout.addView(newTower1);
+                view.animate()
+                        .x(event.getRawX() + dX)
+                        .y(event.getRawY() + dY)
+                        .setDuration(0)
+                        .start();
                 break;
+            default:
+                return false;
         }
-        newLayout.invalidate();
         return true;
+
     }
 
 }
